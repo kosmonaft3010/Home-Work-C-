7f2559098ea711cb1dab4eb5dad4952dfd16ca71
kosmonaft3010 <claud055876@gmail.com>
2022-11-27 18:37:02 +0300

    adedd_home_work

---
+++ HomeWork2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+Console.WriteLine("введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("сумма всех цифр в числе равна: " + GetSumNam(num));
+
+int GetSumNam(int i)
+{
+    int sum = 0;
+
+    while (i > 0)
+    {
+        sum += i % 10;
+        i = i / 10;
+    }
+    return sum;
+}
